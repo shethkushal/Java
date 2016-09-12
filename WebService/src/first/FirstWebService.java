0d1beaae0aa8ce2554c5edc752fb020ba78820f4
Kushal <kushalsheth93@gmail.com>
2016-09-11 21:08:04 -0400

    Update

--- WebService/src/first/FirstWebService.java
+++ WebService/src/first/FirstWebService.java
@@ -1,14 +1,17 @@
 package first;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/first")
 public class FirstWebService {
 
-	@GET
+	/*@GET
 	@Produces(MediaType.TEXT_XML)		//consumes
 	public String helloWorldXML(){
 		
@@ -26,5 +29,17 @@ public class FirstWebService {
 							"<strong>Password:</strong><input type = password name = password/>  "   +
 							"<input type = button value = Submit>";
 		return response;
+	}*/
+	
+	@POST
+	@Path("/login")
+	public Response authUser(
+			@FormParam("Username") String username,
+			@FormParam("Password") String password){
+		
+		return Response.status(200)
+				.entity("Success Username: " )
+				
+				.build();
 	}
 }
